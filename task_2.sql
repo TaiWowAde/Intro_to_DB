9f52163004755af164512a7630cf55b29a9b4bcb
Taiwow <taiwow@MacBookPro.Home>
2025-06-28 13:02:32 +0100

    fixed syntax errors

--- task_2.sql
+++ task_2.sql
@@ -1,38 +1,39 @@
 USE alx_book_store;
 
-CREATE TABLE authors (
+
+CREATE TABLE Authors (
   author_id INT PRIMARY KEY,
-  author_name VARCHAR(215)
+  author_name VARCHAR(215) NOT NULL
 );
 
-CREATE TABLE books (
+CREATE TABLE Books (
   book_id INT PRIMARY KEY,
-  title VARCHAR(130),
-  author_id INT,
+  title VARCHAR(130) NOT NULL,
+  author_id INT NOT NULL,
   price DOUBLE,
   publication_date DATE,
-  FOREIGN KEY (author_id) REFERENCES authors(author_id)
+  FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
-CREATE TABLE customers (
+CREATE TABLE Customers (
   customer_id INT PRIMARY KEY,
-  customer_name VARCHAR(215),
+  customer_name VARCHAR(215) NOT NULL,
   email VARCHAR(215),
   address TEXT
 );
 
-CREATE TABLE orders (
+CREATE TABLE Orders (
   order_id INT PRIMARY KEY,
-  customer_id INT,
+  customer_id INT NOT NULL,
   order_date DATE,
-  FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+  FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE order_details (
+CREATE TABLE Order_Details (
   orderdetailid INT PRIMARY KEY,
-  order_id INT,
-  book_id INT,
+  order_id INT NOT NULL,
+  book_id INT NOT NULL,
   quantity DOUBLE,
-  FOREIGN KEY (order_id) REFERENCES orders(order_id),
-  FOREIGN KEY (book_id) REFERENCES books(book_id)
+  FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+  FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
